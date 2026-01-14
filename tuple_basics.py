9ebfafba700f9be70d68a9e24c5b1fa69553e57a
n4dee <nadeemohamed07@gmail.com>
2026-01-14 20:45:35 +0530

    added  tuples

---
+++ tuple_basics.py
@@ -0,0 +1,7 @@
+subjects = ("Maths", "Physics", "Chemistry")
+print("Tuple:", subjects)
+print("First subject:", subjects[0])
+print("Second subject:", subjects[1])
+print("Number of subjects:", len(subjects))
+
+

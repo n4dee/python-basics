dd375f06820df7b82ca77943e76f04df1ef392aa
Nadeem <nadeemohamed07@gmail.com>
2026-01-18 23:20:06 +0530

    Add functions for addition and squaring numbers

---
+++ functions_intro.py
@@ -0,0 +1,17 @@
+# Functions Introduction Program
+
+# Function to add two numbers
+def add_numbers():
+    a = 10
+    b = 20
+    result = a + b
+    print("Sum:", result)
+
+# Function to find square of a number
+def square_number():
+    n = 5
+    print("Square:", n * n)
+
+# Calling the functions
+add_numbers()
+square_number()

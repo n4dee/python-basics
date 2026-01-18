a9966f429a67e22a2e6e762ab57e9a41db306ab0
Nadeem <nadeemohamed07@gmail.com>
2026-01-18 23:11:55 +0530

    Add dictionary operations example for student data

---
+++ dictionary_operations.py
@@ -0,0 +1,19 @@
+# Dictionary Basics Program
+
+# Creating a dictionary
+student = {
+    "name": "Rahul",
+    "roll_no": 101,
+    "marks": 85
+}
+
+# Printing the entire dictionary
+print("Student Dictionary:", student)
+
+# Accessing values using keys
+print("Name:", student["name"])
+print("Roll Number:", student["roll_no"])
+print("Marks:", student["marks"])
+
+# Length of dictionary
+print("Number of items:", len(student))
